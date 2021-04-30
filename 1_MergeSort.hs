df3e3ee76883db0790eede983558147ae6bd7990
Yusuf <youssef.2012.0@gmail.com>
2021-04-30 04:47:14 +0200

    Code Refactor

--- 1_MergeSort.hs
+++ 1_MergeSort.hs
@@ -2,26 +2,26 @@ import Data.Bifunctor
 
 mergeSort :: [Int] -> [Int] -- the main mergeSort func
 mergeSort [a] = [a]
-mergeSort x = 
+mergeSort x =
     let s = splitSort x
-    in mergedSort (fst s, snd s, [])
+    in uncurry mergedSort s
 splitSort :: [Int] -> ([Int], [Int]) -- takes a list and splits it
 splitSort x =
   let lsts = splitAt (div (length x) 2) x
    in Data.Bifunctor.bimap mergeSort mergeSort lsts
-mergedSort :: ([Int], [Int], [Int]) -> [Int] -- merges the two sorted lists into a single sorted list
-mergedSort ([], [], x) = x
-mergedSort (a, [], x) = x ++ a
-mergedSort ([], b, x) = x ++ b
-mergedSort (a:as, b:bs, x) =
-    if a < b then mergedSort (as, b:bs, x ++ [a]) else mergedSort (a : as, bs, x ++ [b])
+mergedSort :: [Int] -> [Int] -> [Int] -- merges the two sorted lists into a single sorted list
+mergedSort [] [] = []
+mergedSort a [] = a
+mergedSort [] b = b
+mergedSort (a:as) (b:bs) =
+    if a < b then a:mergedSort as (b:bs) else b:mergedSort (a:as) bs
 
 
 testMS = do
-    print $ mergedSort ([], [], [1,2])
-    print $ mergedSort ([3], [], [1,2])
-    print $ mergedSort ([], [3], [1,2])
-    print $ mergedSort ([3], [4], [1,2])
+    print $ mergedSort [] []
+    print $ mergedSort [3] []
+    print $ mergedSort [] [3]
+    print $ mergedSort [3] [4]
 
     print $ mergeSort [2,1]
     print $ mergeSort [5,8,1,3,6,9,8,7,13]
